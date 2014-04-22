8bc5bb93502959a2a49adc559f2c0b977f7cac1a
Mikkel Madsen <m@madsn.net>
2014-04-22 17:55:27 +0200

    Scotty - POST request example

--- exercises/scotty/main.hs
+++ exercises/scotty/main.hs
@@ -13,19 +13,24 @@ main = scottyH' 3000 $ do
   middleware $ staticPolicy (noDots >-> addBase "static")
 
   get "/" index
-  get "/:word" beamMeUp
-
-
-beamMeUp :: ActionH' ()
-beamMeUp = do
-  beam <- param "word"
-  setH "action" $ MuVariable (beam :: String)
-  -- ^ "action" will be binded to the contents of 'beam'
-  hastache "index.html"
-
+  post "/create" createPOST
+  get "/create" createGET
 
 index :: ActionH' ()
 -- ??: Why is the apostrophe needed here? In "regular" Scotty this would be ActionM,
 index = do
   setHeader "Content-Type" "text/html"
   file "views/index.html"
+
+
+createPOST :: ActionH' ()
+createPOST = do
+  name <- param "name"
+  -- Persist to db
+  setH "name" $ MuVariable (name :: String)
+  hastache "show.html"
+
+createGET :: ActionH' ()
+createGET = do
+  setHeader "Content-Type" "text/html"
+  file "views/create.html"
