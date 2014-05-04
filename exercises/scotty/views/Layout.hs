531ff0c46c2a87d0428743c703bff1f143272a1e
Mikkel Madsen <m@madsn.net>
2014-05-04 13:34:13 +0200

    Ups - forgot the layout block

--- exercises/scotty/views/Layout.hs
+++ exercises/scotty/views/Layout.hs
@@ -19,8 +19,28 @@ import           Views.Utils                 (blaze, pet)
 import           Web.Scotty                  (ActionM)
 import           Data.Char                   (toUpper)
 
-navBar :: String -> Html
-navBar activePage = div ! class_ "navbar navbar-default navbar-static-top" $ div ! class_ "container" $ do
+layout :: Html -> Html -> Html
+layout t b = docTypeHtml $ do
+           pet "<!--[if lt IE 7]>      <html class='no-js lt-ie9 lt-ie8 lt-ie7'> <![endif]-->"
+           pet "<!--[if IE 7]>         <html class='no-js lt-ie9 lt-ie8'/> <![endif]-->"
+           pet "<!--[if IE 8]>         <html class='no-js lt-ie9'> <![endif]-->"
+           pet "<!--[if gt IE 8]><!--> <html class='no-js'> <!--<![endif]-->"
+           head $ do
+             title t
+             meta ! charset "utf-8"
+             meta ! httpEquiv "X-UA-Compatible" ! content "IE=edge,chrome=1"
+             meta ! name "description" ! content "Inspire Text"
+             meta ! name "viewport" ! content "width=device-width"
+             link ! href "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" ! rel  "stylesheet" ! media "screen"
+             style $ pet $ toStrict layoutCss
+           body $ do
+             navBar >> b
+             script ! src "//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" $ mempty
+             script ! src "//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js" $ mempty
+
+
+navBar :: Html
+navBar = div ! class_ "navbar navbar-default navbar-static-top" $ div ! class_ "container" $ do
            div ! class_ "navbar-header" $ do
              button ! type_ "button"
                     ! class_ "navbar-toggle" ! dataAttribute "toggle" "collapse" ! dataAttribute "target" ".navbar-collapse" $ do
