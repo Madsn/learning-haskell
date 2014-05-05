cf3482497989f2a220553bf2caf556e70aab2a97
Mikkel Madsen <m@madsn.net>
2014-05-05 22:46:49 +0200

    First computer turn

--- exercises/tic-tac-toe/Main.hs
+++ exercises/tic-tac-toe/Main.hs
@@ -2,8 +2,11 @@ module Main where
 
 import System.Exit (exitSuccess)
 import Data.List (intersperse)
-import Data.Sequence
+import Data.Sequence (fromList, update)
 import Data.Foldable (toList)
+import Control.Monad
+import Control.Monad.Reader
+import Data.Char (digitToInt)
 
 type Board = String
 
@@ -37,11 +40,28 @@ numberRange = "123456789"
 validMoves :: Board -> String
 validMoves b = intersperse ',' [ x | x <- numberRange, x `elem` b ]
 
+firstAvailable :: Board -> Int
+firstAvailable b = subtract 1 . digitToInt $ head $ filter (liftM2 (&&) (/='O') (/='X')) b
+
 playerTurn :: Board -> IO ()
 playerTurn b = do
-    putStrLn $ "Choose one of the available fields: " ++ validMoves b++ "\n"
+    putStrLn $ "Choose one of the available fields: " ++ validMoves b
     move <- getLine
-    putStrLn $ getBoard $ toList $ update (subtract 1 $ read move :: Int) 'O' $ fromList b
+    let newBoard = updateBoard (subtract 1 $ read move :: Int) 'O' b
+    putStrLn $ getBoard newBoard
+    computerTurn newBoard
+
+computerTurn :: Board -> IO ()
+computerTurn b = do
+    putStrLn "Computers turn"
+    let move = firstAvailable b
+    putStrLn $ "Computer chooses: " ++ show move
+    let newBoard = updateBoard move 'X' b
+    putStrLn $ getBoard newBoard
+
+updateBoard :: Int -> Char -> Board -> Board
+updateBoard i c b = toList $ update i c $ fromList b
+
 
 main :: IO ()
 main = do
