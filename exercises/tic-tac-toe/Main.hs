74fa7eab9967a860a6da364b3fa87518683a243d
Mikkel Madsen <m@madsn.net>
2014-05-11 01:23:11 +0200

    Attempting to determine winning/tied game states (not working)

--- exercises/tic-tac-toe/Main.hs
+++ exercises/tic-tac-toe/Main.hs
@@ -15,16 +15,30 @@ data Square = SQ0 | SQ1 | SQ2 | SQ3 | SQ4 | SQ5 | SQ6 | SQ7 | SQ8 deriving (Eq,O
 type GameState = (Set Square, Set Square)
 
 board :: Set Square
-board = Set.fromList [SQ0, SQ1, SQ2, SQ3, SQ4, SQ5, SQ6, SQ7, SQ8]
+board = Set.fromList [SQ0, SQ1, SQ2,
+                      SQ3, SQ4, SQ5,
+                      SQ6, SQ7, SQ8]
+
+winningCombinations :: [Set Square]
+winningCombinations = [Set.fromList [SQ0,SQ1,SQ2],
+                       Set.fromList [SQ0,SQ3,SQ6],
+                       Set.fromList [SQ1,SQ4,SQ7],
+                       Set.fromList [SQ2,SQ5,SQ8],
+                       Set.fromList [SQ3,SQ4,SQ5],
+                       Set.fromList [SQ6,SQ7,SQ8],
+                       Set.fromList [SQ0,SQ4,SQ8],
+                       Set.fromList [SQ2,SQ4,SQ6],
+                       Set.fromList [SQ1,SQ4,SQ7],
+                       Set.fromList [SQ2,SQ5,SQ8]]
 
 validMoves :: GameState -> Set Square
 validMoves gs = board Set.\\ fst gs Set.\\ snd gs
 
 showValidMoves :: GameState -> String
-showValidMoves gs = unwords $ Set.toList $ Set.map squareToNumber $ validMoves gs
+showValidMoves gs = unwords $ Set.toList $ Set.map squareToString $ validMoves gs
 
-squareToNumber :: Square -> String
-squareToNumber s = [last $ show s]
+squareToString :: Square -> String
+squareToString s = [last $ show s]
 
 squareFromString :: String -> Maybe Square
 squareFromString "0" = Just SQ0
@@ -50,7 +64,7 @@ getCell :: Square -> GameState -> String
 getCell s gs
     | Set.member s $ fst gs = "O"
     | Set.member s $ snd gs = "X"
-    | otherwise = squareToNumber s
+    | otherwise = squareToString s
 
 
 getBoard :: GameState -> String
@@ -75,7 +89,6 @@ updateGameState gs s _ = (fst gs, Set.insert s $ snd gs)
 
 handlePlayerMove :: GameState -> Maybe Square -> IO ()
 handlePlayerMove gs (Just s) = do
-    putStrLn $ "Square: " ++ show s
     let newGs = updateGameState gs s 'O'
     computerTurn newGs
 handlePlayerMove gs Nothing = do
@@ -84,6 +97,7 @@ handlePlayerMove gs Nothing = do
 
 playerTurn :: GameState -> IO ()
 playerTurn gs = do
+    checkIfGameOver gs
     putStrLn $ getBoard gs
     putStrLn $ "Choose a cell, enter one of the following: " ++ show (showValidMoves gs)
     input <- getLine
@@ -95,7 +109,38 @@ firstAvailable gs = Set.findMin $ validMoves gs
 
 computerTurn :: GameState -> IO ()
 computerTurn gs = do
+    checkIfGameOver gs
     playerTurn $ updateGameState gs (firstAvailable gs) 'X'
+
+gameTied :: GameState -> Bool
+gameTied gs
+    | Set.null $ validMoves gs = True
+    | otherwise = False
+
+playerWins :: GameState -> Bool
+playerWins gs
+    | Set.null (fst gs) = False
+    | or $ Prelude.map (Set.isSubsetOf (fst gs)) winningCombinations = True
+
+playerLoses :: GameState -> Bool
+playerLoses gs
+    | Set.null (snd gs) = False
+    | or $ Prelude.map (Set.isSubsetOf (snd gs)) winningCombinations = True
+
+checkIfGameOver :: GameState -> IO ()
+checkIfGameOver gs =
+    if playerWins gs then do
+        putStrLn "Player wins!"
+        putStrLn $ show $ fst gs
+        startGame
+    else if playerLoses gs then do
+        putStrLn "Player loses!"
+        startGame
+    else if gameTied gs then do
+        putStrLn "Game tied!"
+        startGame
+    else
+        putStrLn ""
 
 main :: IO ()
 main = do
@@ -105,13 +150,27 @@ main = do
 
 startGame :: IO ()
 startGame = do
+    putStrLn "Do you wish to start a new game? (y/n)"
+    input <- getLine
+    if input == "y" then do
+        newGame
+    else if input == "n" then
+        exitSuccess
+    else do
+        putStrLn "Invalid input."
+        newGame
+
+newGame :: IO ()
+newGame = do
     let gs = (Set.empty, Set.empty)
-    putStrLn "Do you wish to start? (y/n)"
-    playerStarts <- getLine
-    if playerStarts == "y" then
+    putStrLn "Player or computer starts? (p/c)"
+    whoStarts <- getLine
+    if whoStarts == "p" then
         playerTurn gs
-    else if playerStarts == "n" then
+    else if whoStarts == "c" then
         computerTurn gs
     else do
-        putStrLn "Invalid input"
+        putStrLn "Invalid input."
         startGame
+
+
