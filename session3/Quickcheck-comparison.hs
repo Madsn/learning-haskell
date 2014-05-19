bca765acc6a1076f0de74185e74ae81fd8b02b03
Mikkel Madsen <m@madsn.net>
2014-05-19 19:18:30 +0200

    comparing clamp implementations with quickcheck

---
+++ session3/Quickcheck-comparison.hs
@@ -0,0 +1,19 @@
+module Clamptest where
+
+import Data.List (sort)
+import Test.QuickCheck
+
+
+clampA :: Int -> Int -> Int -> Int
+clampA minBound maxBound x = (!! 1) . sort $ [minBound,maxBound,x]
+
+clampB :: Int -> Int -> Int -> Int
+clampB minBound maxBound x
+    | x > maxBound = maxBound
+    | x < minBound = minBound
+    | otherwise = x
+
+main = quickCheck (\i -> \j -> \k -> i < j ==> clampA i j k == clampB i j k)
+
+--main = quickCheck (\i -> \j -> \k -> clampA i j k == clampB i j k)
+  -- conclusion: they are not in fact equivalent, when given conflicting min/max bound values (precedence differs)
