eb055c8c00ff26fa3763ea894f76dfe90be8d92e
Mikkel Madsen <m@madsn.net>
2014-05-21 22:33:02 +0200

    server sync of changes to team score

--- exercises/ranklists/public/js/scoreboard/scoreboard-controller.js
+++ exercises/ranklists/public/js/scoreboard/scoreboard-controller.js
@@ -1,24 +1,58 @@
 'use strict';
 
 angular.module('ranklists')
-  .controller('ScoreboardController', ['$scope', '$filter', 'resolvedTeam', 'Team',
-    function ($scope, $filter, resolvedTeam, Team) {
+  .controller('ScoreboardController', ['$scope', '$filter', 'Team',
+    function ($scope, $filter, Team) {
 
-      $scope.teams = resolvedTeam;
+      $scope.teams = Team.query(function (objs) {
+              // deal with Haskell Persistent json format
+              for (var i = 0; i < objs.length; i++) {
+                var obj = objs[i];
+                obj.id = obj.key;
+                delete obj.key;
+                for (var prop in obj.value) {
+                  obj[prop] = obj.value[prop];
+                }
+                delete obj.value;
+              }
+            });
 
       $scope.incrementScore = function(id){
-        var team = $filter('getById')($scope.teams, id)
-        team.score = team.score+1;
+        changeScore(id, +1);
       };
 
       $scope.decrementScore = function(id){
-        var team = $filter('getById')($scope.teams, id)
-        team.score = team.score-1;
+        changeScore(id, -1);
       };
 
       $scope.resetScore = function(id){
+        setScore(id, 0);
+      };
+
+      var changeScore = function(id, val){
         var team = $filter('getById')($scope.teams, id)
-        team.score = 0;
+        team.score = team.score + val;
+        Team.update({id: id}, team);
+      };
+
+      var setScore = function(id, score){
+        var team = $filter('getById')($scope.teams, id)
+        team.score = score;
+        Team.update({id: id}, team);
+      };
+
+      var transformObjs = function (objs) {
+        // deal with Haskell Persistent json format
+        for (var i = 0; i < objs.length; i++) {
+          var obj = objs[i];
+          obj.id = obj.key;
+          delete obj.key;
+          for (var prop in obj.value) {
+            obj[prop] = obj.value[prop];
+          }
+          delete obj.value;
+          console.log(obj);
+        }
       };
 
     }]).filter('getById', function() {
--- exercises/ranklists/public/js/scoreboard/scoreboard-router.js
+++ exercises/ranklists/public/js/scoreboard/scoreboard-router.js
@@ -5,22 +5,6 @@ angular.module('ranklists')
     $routeProvider
       .when('/scoreboard', {
         templateUrl: 'views/scoreboard/scoreboard.html',
-        controller: 'ScoreboardController',
-        resolve:{
-          resolvedTeam: ['Team', function (Team) {
-            return Team.query(function (objs) {
-              // deal with Haskell Persistent json format
-              for (var i = 0; i < objs.length; i++) {
-                var obj = objs[i];
-                obj.id = obj.key;
-                delete obj.key;
-                for (var prop in obj.value) {
-                  obj[prop] = obj.value[prop];
-                }
-                delete obj.value;
-              }
-            });
-          }]
-        }
+        controller: 'ScoreboardController'
       })
     }]);
