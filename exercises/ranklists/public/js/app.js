b1fae36beb676b88a7ea6aaf27db98a9eeda575d
Mikkel Madsen <m@madsn.net>
2014-05-21 22:23:50 +0200

    Increment/decrement score - client side only

--- exercises/ranklists/public/js/app.js
+++ exercises/ranklists/public/js/app.js
@@ -3,7 +3,8 @@ angular.module('ranklists', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date']
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/home/home.html', 
+        templateUrl: 'views/home/home.html',
         controller: 'HomeController'})
       .otherwise({redirectTo: '/'});
   }]);
+
--- exercises/ranklists/public/js/scoreboard/scoreboard-controller.js
+++ exercises/ranklists/public/js/scoreboard/scoreboard-controller.js
@@ -1,9 +1,34 @@
 'use strict';
 
 angular.module('ranklists')
-  .controller('ScoreboardController', ['$scope', 'resolvedTeam',
-    function ($scope, resolvedTeam) {
+  .controller('ScoreboardController', ['$scope', '$filter', 'resolvedTeam', 'Team',
+    function ($scope, $filter, resolvedTeam, Team) {
 
       $scope.teams = resolvedTeam;
 
-    }]);
+      $scope.incrementScore = function(id){
+        var team = $filter('getById')($scope.teams, id)
+        team.score = team.score+1;
+      };
+
+      $scope.decrementScore = function(id){
+        var team = $filter('getById')($scope.teams, id)
+        team.score = team.score-1;
+      };
+
+      $scope.resetScore = function(id){
+        var team = $filter('getById')($scope.teams, id)
+        team.score = 0;
+      };
+
+    }]).filter('getById', function() {
+      return function(input, id) {
+        var i=0, len=input.length;
+        for (; i<len; i++) {
+          if (+input[i].id == +id) {
+            return input[i];
+          }
+        }
+        return null;
+      }
+});
